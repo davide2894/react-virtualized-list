35d0a616cb7edd85dbc0fb122584945871c36b55
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-08-30 00:37:14 +0200

    feat: handle data on localStorage

---
+++ src/data/localDatabase.ts
@@ -0,0 +1,28 @@
+import { localDbKey } from "@/constants";
+import { Row } from "@/types";
+
+export function initLocalDb(data: Array<Row>) {
+  localStorage.setItem(localDbKey, JSON.stringify(data ? data : []));
+}
+
+export function getCurrentDb() {
+  const currentDb = JSON.parse(localStorage.getItem(localDbKey) || "[]");
+  if (currentDb.length) {
+    return currentDb;
+  }
+}
+
+export function addEntryToDb(entry: Row) {
+  const currentDb = getCurrentDb();
+  currentDb.unshift(entry);
+  localStorage.setItem(localDbKey, JSON.stringify(currentDb));
+}
+
+export function updateLocalDb(updatedData: Array<Row>) {
+  localStorage.setItem(localDbKey, JSON.stringify(updatedData));
+}
+
+export function getUpdatedLocalDb(updatedData: Array<Row>) {
+  localStorage.setItem(localDbKey, JSON.stringify(updatedData));
+  getCurrentDb();
+}
