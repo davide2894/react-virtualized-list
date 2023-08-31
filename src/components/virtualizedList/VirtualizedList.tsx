8c61c161b504e51051be30225bed63c5441f0636
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-08-31 22:21:07 +0200

    style: VirtualizedList - add style to component with css modules

--- src/components/virtualizedList/VirtualizedList.tsx
+++ src/components/virtualizedList/VirtualizedList.tsx
@@ -1,19 +1,23 @@
-import { Row } from "@/types";
-import log from "@/utils/log";
-import React, { useState, useEffect, Key, useRef, useCallback } from "react";
-import MemoizedListItem from "../listItem/ListItem";
-import { useThrottle } from "@uidotdev/usehooks";
-import MemoizedBackToTopButton from "../backToTopButton/BackToTopButton";
-import AddNewRowButton from "../addNewRowButton/AddNewRowButton";
-import { useAppSelector } from "@/customHooks/useAppSelector";
+import { Row } from "@types";
+import log from "@utils/log";
+import { useState, Key, useRef, useCallback } from "react";
+import MemoizedListItem from "@components/listItem/ListItem";
+import { useThrottle } from "../../vendor/hooks/useThrottle";
+import MemoizedBackToTopButton from "@components/backToTopButton/BackToTopButton";
+import AddNewRowButton from "@components/addNewRowButton/AddNewRowButton";
+import { loadMoreDataThunk } from "@loadMoreDataThunk";
+import { useAppDispatch } from "@customHooks/useAppDispatch";
+import styles from "./VirtualizedList.module.css";
 
 const VirtualizedList = (props: { data: Array<Row> }) => {
   const [scrollTop, setScrollTop] = useState(0);
+  const dispatch = useAppDispatch();
   const divRef = useRef<HTMLInputElement>(null);
+  const dataLength = props.data.length;
   const containerHeight = 400;
   const itemHeight = 50;
   const throttledScrollTop = useThrottle(scrollTop, 500);
-
+  const loadMoreThreshold = 30;
   const startIndex = Math.floor(throttledScrollTop / itemHeight);
   const endIndex = Math.min(
     props.data.length - 1,
@@ -23,8 +27,32 @@ const VirtualizedList = (props: { data: Array<Row> }) => {
 
   const handleScroll = (e: any) => {
     setScrollTop(e.target.scrollTop);
+    log();
     log("handleScroll called");
   };
+
+  function handleInfiniteLoad() {
+    if (
+      divRef &&
+      divRef.current &&
+      divRef.current.dataset &&
+      divRef.current.dataset.endindex
+    ) {
+      log({
+        dataLength: props.data.length,
+        endIndex: divRef.current.dataset.endindex,
+      });
+      const isNearEnd =
+        dataLength - 1 - parseInt(divRef.current.dataset.endindex) <=
+        loadMoreThreshold;
+      log({
+        isNearEnd,
+      });
+      if (isNearEnd) {
+        dispatch(loadMoreDataThunk({}));
+      }
+    }
+  }
 
   const cachedOnClick = useCallback(() => {
     if (divRef && divRef.current && divRef.current.scrollTo) {
@@ -33,30 +61,38 @@ const VirtualizedList = (props: { data: Array<Row> }) => {
   }, []);
 
   return (
-    <>
+    <div
+      className={styles.container}
+      data-testid="virtualizedListTestAttribute">
       <AddNewRowButton />
       <div
+        data-testid="scrollContainerTestAttribute"
+        data-endindex={endIndex}
         ref={divRef}
         style={{
-          height: containerHeight,
           overflowY: "scroll",
+          height: containerHeight,
         }}
-        onScroll={handleScroll}>
+        onScroll={(e) => {
+          handleScroll(e);
+          handleInfiniteLoad();
+        }}>
         <div
           style={{
             height: `${props.data.length * itemHeight}px`,
           }}>
           <table
+            className={styles.table}
             style={{
               position: "relative",
               height: `${visibleItems.length * itemHeight}px`,
               top: `${startIndex * itemHeight}px`,
             }}>
-            <thead>
+            <thead className={styles.thead}>
               <tr>
-                <th>Column 1</th>
-                <th>Column 2</th>
-                <th>Column 3</th>
+                <th className={styles.th}>Column 1</th>
+                <th className={styles.th}>Column 2</th>
+                <th className={styles.th}>Column 3</th>
               </tr>
             </thead>
             <tbody>
@@ -75,7 +111,7 @@ const VirtualizedList = (props: { data: Array<Row> }) => {
         </div>
         <MemoizedBackToTopButton onClick={cachedOnClick} />
       </div>
-    </>
+    </div>
   );
 };
 
