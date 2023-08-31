87632770914e8b594b601853b85c2ee19c34984a
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-08-31 22:28:05 +0200

    refactor: data.ts - centralize all data logic in one file

--- src/data/data.ts
+++ src/data/data.ts
@@ -4,11 +4,23 @@ import { faker } from "@faker-js/faker";
 import { v4 as uuidv4 } from "uuid";
 
 export const generateFakeData = (numberOfEntries: number) => {
-  return new Promise((resolve) => {
+  return new Promise<Array<Row>>((resolve) => {
     let data: Array<Row> = [];
     for (let i = 0; i < numberOfEntries; i++) {
       data.push(generateFakeDataEntry(i));
     }
     resolve(data);
   });
+};
+
+export const generateFakeDataEntry = (positionInDataset: number = 0) => {
+  const newEntry = {
+    name: faker.commerce.productName(),
+    description: faker.lorem.sentence(),
+    price: faker.commerce.price(),
+    position: positionInDataset,
+    id: uuidv4(),
+  } as unknown as Row;
+
+  return newEntry;
 };
